db39d4d13ba1ac7ddbe5e7d60b97a5852628b61d
DragonsK8 <jrholder1@outlook.com>
2025-01-12 21:59:41 -0500

    quality of life + added scrolling through qr codes

--- qrcode-creator.js
+++ qrcode-creator.js
@@ -6,8 +6,8 @@ var codes = [];
 // create a placeholder qr code 
 
 var qrCode = new QRCode(document.getElementById("qrcode"), {
-  width:window.innerWidth / 4,
-  height:window.innerWidth / 4,
+  width:150,
+  height:150,
   text:""
 });
 
@@ -16,39 +16,78 @@ function generateCode() {
   
   // create a string to store the form data
   var codeText = "";
-
-    // store the form data
-    for (let i = 1; i < 3; i++) {
-      var temp = document.getElementById("data" + i).value;
-      
-      codeText += temp + " ";
-    }
+  
+  // store the form data
+  for (let i = 1; i < 3; i++) {
+    var temp = document.getElementById("data" + i).value;
     
-    // store the form data 
-    codes.push(codeText)
-    
-    // set the current form displayed to the most recently created one
-    showCode(codes.length - 1);
+    codeText += temp + " ";
   }
   
-  // displays the stored qr code of the given number on the page
-  function showCode(number) {
-    // make sure that the qr code exists 
-    if (number < 0 || number >= codes.length) return;
-    // remove the previous QR code
-    
-    // add the code of the given number to the page
-    qrCode.makeCode(codes[number]);
-  }
+  // store the form data 
+  codes.push(codeText)
   
-  // create event listeners
-  
+  // set the current form displayed to the most recently created one
+  codeNumber = codes.length - 1;
+  showCurrentCode();
+}
 
-  // submit button
-  document.getElementById("submit").addEventListener("click", function () {
-    // create the new code
-    generateCode();
-    
-    // show the code and submit button
-    document.getElementById("codes").style.visibility = "visible";
-  });
+// displays the stored qr code of the given number on the page
+function showCurrentCode() {
+  // make sure that the qr code exists 
+  if (codeNumber < 0 || codeNumber >= codes.length) return;
+  // remove the previous QR code
+  
+  // add the code of the given number to the page
+  qrCode.makeCode(codes[codeNumber]);
+}
+
+// create event listeners
+
+// submit button
+document.getElementById("submit").addEventListener("click", function () {
+  // create the new code
+  generateCode();
+  
+  // show the code
+  document.getElementById("qrcode").style.visibility = "visible";
+
+  // display the previous button if needed
+  if (codeNumber != 0) 
+    document.getElementById("previous").style.visibility = "visible";
+  
+});
+
+// previous button
+document.getElementById("previous").addEventListener("click", function () {
+  // go back one code in the list
+  codeNumber--;
+
+  // regenerate the current code
+  showCurrentCode();
+
+  // make the next button visible
+  document.getElementById("next").style.visibility = "visible";
+
+  // check if previous should remain visible
+  if (codeNumber == 0) {
+    document.getElementById("previous").style.visibility = "hidden";
+  }
+});
+
+// next button
+document.getElementById("next").addEventListener("click", function () {
+  // go forward one code in the list
+  codeNumber++;
+
+  // regenerate the current code
+  showCurrentCode();
+
+  // make the previous button visible
+  document.getElementById("previous").style.visibility = "visible";
+
+  // check if next should remain visible
+  if (codeNumber == codes.length - 1) {
+    document.getElementById("next").style.visibility = "hidden";
+  }
+});
