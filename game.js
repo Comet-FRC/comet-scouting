1e7ca32a092d793c4df69a250a227f5d46781a1b
DragonsK8 <jrholder1@outlook.com>
2025-01-13 21:28:26 -0500

    fixed timer inaccuracy

--- game.js
+++ game.js
@@ -1,75 +1,83 @@
 var gameTimer = 0;
-var endTime = 15000;
+const endTime = 1500;
 var gameEnded = true;
+var start;
+var timerInterval;
 
 var codeText = "";
 
 function beginGame() {
-    // change the start button to the end button
-    var startButton = document.getElementById("starter");
-    startButton.value = "End Game";
+  // change the start button to the end button
+  var startButton = document.getElementById("starter");
+  startButton.value = "End Game";
 
-    // make the timer visible
-    var timer = document.getElementById("timer");
-    timer.style.visibility = "visible";
+  // make the timer visible
+  var timer = document.getElementById("timer");
+  timer.style.visibility = "visible";
 
-    // make the timer display the game time
-    var timerDisplay = document.getElementById("timerDisplay");
-    timerDisplay.innerText = gameTimer;
+  // make the timer start at 0
+  var timerDisplay = document.getElementById("timerDisplay");
+  timerDisplay.innerText = 0;
 
-    // initialize timer value
-    gameTimer = 0;
+  // initialize timer values
+  gameTimer = 0;
+  gameEnded = false;
+  start = Date.now();
 
-    // begin timer counting up
-    gameEnded = false;
-    setTimeout(function() { updateTimer() }, 10)
+  // start timer counting
+  timerInterval = setInterval(updateTimer, 100)
+  start = Date.now();
+  
 }
 
+// adds an event of the given type to the String used to generate the qr code
 function appendEvent(eventType) {
-    // add the event type
-    codeText += eventType;
+  // add the event type
+  codeText += eventType;
 
-    // add the current time of the event
-    codeText += convert(gameTimer);
+  // add the current time of the event
+  codeText += convert(gameTimer);
 }
 
 function endGame() {
-    // change the end button to the start button
-    var startButton = document.getElementById("starter");
-    startButton.value = "Start Game";
+  // change the end button to the start button
+  var startButton = document.getElementById("starter");
+  startButton.value = "Start Game";
 
-    // stop the timer from counting up
-    gameEnded = true;
+  // stop the timer from counting up
+  gameEnded = true;
+  clearInterval(timerInterval);
 }
 
 function updateTimer() {
-    // increment timer
-    gameTimer += 1;
+  // keep track of time elapsed since start
+  let delta = Date.now() - start;
+  gameTimer = Math.floor(delta / 100);
 
-    // update timer display
-    var timerDisplay = document.getElementById("timerDisplay");
-    timerDisplay.innerText = gameTimer / 100.0;
-    
-    // if game is still going, continue 
-    if (gameTimer < endTime && !gameEnded) {
-        setTimeout(function() { updateTimer() }, 10);
-    }
+  // update timer display
+  var timerElem = document.getElementById("timerDisplay");
+  timerElem.innerText = gameTimer / 10.0;
+
+  // end the game if it's over
+  if (gameTimer >= endTime + 600 || gameEnded) {
+    endGame();
+  }
 }
 
 // event listeners
 
 document.getElementById("starter").addEventListener("click", function () {
-    if (gameEnded) 
-        beginGame();
-    else 
-        endGame();
+  if (gameEnded)
+    beginGame();
+  else
+    endGame();
 });
 
 // coral buttons
 for (let i = 1; i <= 4; i++) {
-    document.getElementById("l" + i).addEventListener("click", function() {
-        if (!gameEnded) {
-            appendEvent(i);
-        }
-    });
+  document.getElementById("l" + i).addEventListener("click", function() {
+    if (!gameEnded) {
+      appendEvent(i);
+    }
+  });
 }
