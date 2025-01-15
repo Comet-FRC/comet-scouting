c385d65201f794782616a4d825c0cb94985e0a74
DragonsK8 <jrholder1@outlook.com>
2025-01-14 19:14:38 -0500

    started implementing tag button

---
+++ form.js
@@ -0,0 +1,17 @@
+
+
+// tags button
+document.getElementById("tag-select").addEventListener("click", function () {
+  
+  // show the different tag options
+  var tagSelect = document.getElementById("tag-options");
+  tagSelect.removeAttribute("hidden");
+  tagSelect.size = tagSelect.length;
+  
+  // record options when clicked
+  tagSelect.addEventListener("click", function () {
+    
+    var tagDisplay = document.getElementById("selected-tags");
+    tagDisplay.innerText += parseInt()
+  });
+});
