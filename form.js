51d7f59f978da0be233ebfc438e107f4c0ef5690
DragonsK8 <jrholder1@outlook.com>
2025-01-15 22:58:56 -0500

    simplified tag structure + temporarily removed functionality

--- form.js
+++ form.js
@@ -1,79 +1,17 @@
-var tags = [];
-var tagsExpanded = false;
-
-// tags button
-document.getElementById("tag-select").addEventListener("click", function () {
-  
-  // show the different tag options
-  var tagSelect = document.getElementById("tag-options");
-  tagSelect.removeAttribute("hidden");
-  tagSelect.size = tagSelect.length;
-  
-  // expand or contract the tags 
-  if (tagsExpanded) {
-    tagSelect.hidden = true;
-  }
-  
-  tagsExpanded = !tagsExpanded;
-  
-  
-  // record options when clicked
-  tagSelect.addEventListener("click", function () {
-    // store the option clicked
-    let temp = tagSelect.options[tagSelect.selectedIndex].text;
-    
-    // check if the option clicked has already been tagged
-    if (tags.includes(temp)) {
-      // remove it if it does
-      let matchesTemp = (element) => element == temp;
-      tags.splice(tags.findIndex(matchesTemp), 1);
-      
-      // mark the tag as untagged
-      tagSelect.options[tagSelect.selectedIndex].removeAttribute("class");
-    }
-    else {
-      // otherwise store it in tags
-      tags.push(temp);
-      
-      // mark the selected tag as tagged
-      tagSelect.options[tagSelect.selectedIndex].className = "tagged";
-    }
-    
-    // clear currently shown tags
-    var tagDisplay = document.getElementById("selected-tags");
-    tagDisplay.innerText = "";
-    
-    
-    // get the locations of elements
-    let indices = Object.keys(tags);
-    
-    // for each element add the item to the displayed tags
-    indices.forEach(function(item) {
-      let elem = document.createElement("span");
-      elem.innerText = tags[item];
-      
-      tagDisplay.appendChild(elem);
-    });
-
-    // if there are no tags display "None"
-    if (tagDisplay.innerText == "") {
-      let elem = document.createElement("span");
-      elem.innerText = "None";
-
-      tagDisplay.appendChild(elem);
-    }
-  });
+// tags button 
+document.getElementById("strength-button").addEventListener("click", function () {
+  let strengthList = document.getElementById("strength-list");
+  strengthList.classList.toggle("show");
 });
 
-// check if the tags are expanded
-if (tagsExpanded) {
-  // check if the document is clicked anywhere else
-  document.addEventListener("click", event => {
-    let isClickInside = document.getElementById("tag-options").contains(event.target);
-    
-    if (!isClickInside) {
-      tagsExpanded = false;
-      document.getElementById("tag-options").hidden = "true";
+// check if anywhere else is clicked
+window.onclick = function(event) {
+  console.log(event.target.id + " clicked!");
+  if (!event.target.matches('#strength-button')) {
+    let dropdown = document.getElementById("strength-list");
+
+    if (dropdown.classList.contains("show")) {
+      dropdown.classList.remove("show");
     }
-  })
-}
+  }
+};
