203cbf391035a1ec527e81a81b5f4a289c91596c
DragonsK8 <jrholder1@outlook.com>
2025-01-16 16:04:49 -0500

    added tag style + actual functionality

--- form.js
+++ form.js
@@ -1,17 +1,85 @@
-// tags button 
-document.getElementById("strength-button").addEventListener("click", function () {
-  let strengthList = document.getElementById("strength-list");
-  strengthList.classList.toggle("show");
-});
+// get all dropdowns in the document
+var dropdowns = document.getElementsByClassName("tag-dropdown");
+
+function addTag(dropdownItem) {
+  // create a variable to store the display that corresponds to this element
+  let myDisplay = null;
+  
+  // find it
+  let displays = document.getElementsByClassName("tag-display");
+  for (let i = 0; i < displays.length; i++) {
+    if (displays.item(i).tag == dropdownItem.parentElement.tag) {
+      myDisplay = displays.item(i);
+    }
+  }
+
+  // move the element to the display we just found
+  myDisplay.appendChild(dropdownItem);
+
+  // update the event listener
+  dropdownItem.removeEventListener("click", function () { addTag(tag) });
+  dropdownItem.addEventListener("click", function () { removeTag(dropdownItem) })
+}
+
+function removeTag(tag) {
+  // create a variable to store the dropdown that corresponds to this element
+  let myDropdown = null;
+
+  // find it
+  for (let i = 0; i < dropdowns.length; i++) {
+    if (dropdowns.item(i).tag == tag.parentElement.tag) {
+      myDropdown = dropdowns.item(i);
+    }
+  }
+
+  // move the element to the display we just found
+  myDropdown.appendChild(tag);
+
+  // update the event listener
+  tag.removeEventListener("click", function () { removeTag(dropdownItem) });
+  tag.addEventListener("click", function () { addTag(tag) })
+}
+
+function showDropdown(currentButton) {
+  for (let i = 0; i < dropdowns.length; i++) {
+    if (dropdowns.item(i).tag == currentButton.tag) {
+      dropdowns.item(i).classList.toggle("show");
+    }
+  }
+}
+
+// tag buttons 
+for (let i = 0; i < document.getElementsByClassName("tag-button").length; i++) {
+  // get the current button
+  let currentButton = document.getElementsByClassName("tag-button").item(i);
+  currentButton.addEventListener("click", function () { showDropdown(currentButton) });
+}
+
+// // tags button 
+// document.getElementById("strength-button").addEventListener("click", function () {
+//   let strengthList = document.getElementById("strength-dropdown");
+//   strengthList.classList.toggle("show");
+// });
+
+
+
+// add event listeners to all dropdown items
+for (let i = 0; i < dropdowns.length; i++) {
+  for (let j = 0; j < dropdowns.item(i).childNodes.length; j++) {
+    let dropdownItem = dropdowns.item(i).childNodes.item(j);
+    dropdownItem.addEventListener("click", function () { addTag(dropdownItem) });
+  }
+}
 
 // check if anywhere else is clicked
 window.onclick = function(event) {
-  console.log(event.target.id + " clicked!");
-  if (!event.target.matches('#strength-button')) {
-    let dropdown = document.getElementById("strength-list");
-
-    if (dropdown.classList.contains("show")) {
-      dropdown.classList.remove("show");
+  if (!event.target.matches('.tag-button')) {
+    // loop through all dropdowns in the document
+    for (let i = 0; i < dropdowns.length; i++) {
+      // if the dropdown is shown hide it
+      if (dropdowns.item(i).classList.contains("show")) {
+        dropdowns.item(i).classList.remove("show");
+      }
     }
   }
 };
