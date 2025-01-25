849dd760196aa4d5251e15c4de5a4462ebf452ad
DragonsK8 <jrholder1@outlook.com>
2025-01-25 10:06:20 -0500

    minus button functionality

--- game.js
+++ game.js
@@ -11,25 +11,25 @@ function beginGame() {
   var startButton = document.getElementById("starter");
   startButton.value = "End Game";
   pressChange(startButton);
-
+  
   // make the timer visible
   var timer = document.getElementById("timer");
   timer.style.visibility = "visible";
-
+  
   // make the timer start at 0
   var timerDisplay = document.getElementById("timerDisplay");
   timerDisplay.innerText = 0;
-
+  
   // make coral points start at 0
   for (let i = 1; i < 5; i++) {
     document.getElementById("l" + i + "-score").innerText = 0;
   }
-
+  
   // initialize timer values
   gameTimer = 0;
   gameEnded = false;
   start = Date.now();
-
+  
   // start timer counting
   timerInterval = setInterval(updateTimer, 100)
   start = Date.now();
@@ -47,9 +47,21 @@ function pressChange(button) {
 function appendEvent(eventType) {
   // add the event type
   codeText += eventType;
-
+  
   // add the current time of the event
   codeText += convert(gameTimer, 2);
+}
+
+// removes an event of the given type to the String used to generate the qr code
+function removeEvent(eventType) {
+  // find the last event of the given type
+  for (var i = codeText.length - 1; i >= 0; i--) {
+    if (codeText.charAt(i) == eventType && i % 3 == 0)  {
+      // remove the event
+      codeText = codeText.substring(0, i) + codeText.substring(i + 3);
+      return;
+    }
+  }
 }
 
 function endGame() {
@@ -57,7 +69,7 @@ function endGame() {
   var startButton = document.getElementById("starter");
   startButton.value = "Start Game";
   pressChange(startButton);
-
+  
   // stop the timer from counting up
   gameEnded = true;
   clearInterval(timerInterval);
@@ -67,11 +79,11 @@ function updateTimer() {
   // keep track of time elapsed since start
   let delta = Date.now() - start;
   gameTimer = Math.floor(delta / 100);
-
+  
   // update timer display
   var timerElem = document.getElementById("timerDisplay");
   timerElem.innerText = gameTimer / 10.0;
-
+  
   // end the game if it's over
   if (gameTimer >= endTime + 600 || gameEnded) {
     endGame();
@@ -84,7 +96,7 @@ document.getElementById("starter").addEventListener("click", function () {
   if (gameEnded)
     beginGame();
   else
-    endGame();
+  endGame();
 });
 
 // coral buttons
@@ -97,6 +109,20 @@ for (let i = 1; i <= 4; i++) {
       pressChange(document.getElementById("l" + i));
     }
   });
+}
+
+// coral minus buttons
+for (let i = 1; i <= 4; i++) {
+  document.getElementById("minus" + i).addEventListener("click", function() {
+    if (!gameEnded) {
+      // subtract one from the score display
+      score = document.getElementById("l" + i + "-score");
+      score.innerText = Math.max(parseInt(score.innerText) - 1, 0);
+      
+      // remove the event from  the qr code
+      removeEvent("" + i);
+    }
+  })
 }
 
 // algae buttons
